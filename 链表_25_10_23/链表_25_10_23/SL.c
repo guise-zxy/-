a8579f10ace01940406ae892a56c9885942805e4
giuse <genius200612zhxy@qq.com>
2025-10-28 22:40:49 +0800

    第三次链表全部方法提交

--- 链表_25_10_23/链表_25_10_23/SL.c
+++ 链表_25_10_23/链表_25_10_23/SL.c
@@ -10,8 +10,15 @@
 SLNode* SLBuy(SLData x)
 {
 	SLNode* node = (SLNode*)malloc(sizeof(SLNode));
-	node->next = NULL;
+	// 只用用了一次malloc，都要在之后去判断返回的地址是否为NULL，这是好习惯。
+	if (node == NULL)
+	{
+		assert(node);
+		
+	}
+	
 	node->data = x;
+	node->next = NULL;
 	return node;
 }
 
@@ -31,6 +38,8 @@ void SLPrint(SLNode * head)
 		
 	
 	} while (cur=cur->next);
+
+	printf("\n");
 
 }
 
@@ -67,3 +76,206 @@ void Pushback(SLNode** pphead, SLData x)
 
 }
 
+//5.头插
+
+void Pushfront(SLNode** pphead, SLData x)
+{
+	assert(pphead);
+	SLNode* newnode = SLBuy(x);
+	newnode->next = *pphead;
+	*pphead = newnode;
+}
+
+
+
+//6。尾删
+void Popback(SLNode** pphead)
+{
+	assert(pphead && *pphead);
+	if ((*pphead)->next == NULL)  //判断链表是否只有一个元素？是的话就要修改 *pphead
+	{
+		free(*pphead);
+		*pphead = NULL;
+
+	}
+	else              //链表有2个元素以上，直接开始找尾部元素
+	{
+
+		//遍历元素，找到要删的尾部元素  并且 记录 尾部元素的前一个元素 pre 
+		SLNode* cur = *pphead;
+		SLNode* pre = *pphead; 
+		
+		//注意先后顺序不能调换，否则万一cur是NULL，那直接解引用的话是会报错的。
+		while (cur->next)
+		{
+			pre = cur;
+			cur = cur->next;
+		}
+
+		//此时跳出循环之后，当前的cur所指向的就是 尾部元素,pre就是指向前一个元素。
+		free(cur);
+		cur = NULL;
+		pre->next = NULL;
+
+	}
+
+}
+
+//7.头删
+void Popfront(SLNode** pphead)
+{
+	assert(pphead && *pphead);
+	   //存放初始头节点的下个元素的地址，方便最后更新头节点；
+	SLNode* temp = (*pphead)->next;
+	//将旧的头节点的next指针指向为空，再free；
+	//(*pphead)->next = NULL;           这行不加也行
+	free(*pphead);
+	(*pphead) = temp;
+}
+
+//8.查找元素
+SLNode* SLFind(SLNode** pphead, SLData x)
+{
+	assert(pphead && *pphead);
+	SLNode* cur=*pphead;
+	while (cur)
+	{
+		if (cur->data == x)
+		{
+			return cur;
+		}
+		else
+		{
+			cur = cur->next;
+		}
+	}
+	return NULL;
+}
+
+
+//9.在指定位置pos之前插入元素；你必须得找到pre，但是单向链表是不能通过pos来找到pre的；所以得传头节点指针来遍历寻找删除指定pos位置的元素。
+
+void SLInsertBefore(SLNode** pphead, SLNode* pos, SLData x)
+{
+	assert(pphead && pos&& *pphead);
+	assert(*pphead); //也可以加上 判断 *pphead，但是因为 所完成的功能，所以用户在逻辑上不会传个空链表；   但也要注意；
+	SLNode* node = SLBuy(x);
+
+	//如果*pphead==pos(包含了只有一个元素的情况)，要单独拿出来处理，因为它没有 pre 元素 并且 要修改当前链表的头节点*pphead。
+	if (*pphead==pos)
+	{
+		node->next = pos;
+		*pphead = node;
+	}
+	// 多个元素时
+	else
+	{
+
+		//找pos位置元素，并且要记录 pos 的前面一个元素 pre;
+		SLNode* pre = *pphead;
+		SLNode* cur = *pphead;
+		while ((cur != NULL) && (cur != pos))
+		{
+			pre = cur;
+			cur = cur->next;
+		}
+		//按理来说调用这个函数说明一定能找到pos位置上的元素，但是以防万一还是加上判断当前cur是否为NULL；
+		//正常此时cur为pos，开始进行 插入 操作；
+		pre->next = node;
+		node->next = pos;
+
+	}
+
+
+
+}
+
+//10.在指定位置pos 之后插入元素,,,,,,,，此时可以不传头节点的地址，，，直接通过pos来 删除;又或者可以解释为你想删除pos元素，那你必须得找到pre，但是单向链表是不能通过pos来找到pre的；所以得传头节点指针来遍历寻找
+void SLInsertAfter( SLNode* pos, SLData x)
+{
+	assert(pos);
+	SLNode* node = SLBuy(x);
+
+	////找到pos位置                                      //可以不找pos在哪，直接拿到pos修改，此处为冗余代码。
+	//SLNode* cur = *pphead;
+	//while ((cur != NULL) && cur != pos)
+	//{
+	//	cur = cur->next;
+	//}
+
+	//出了循环，此时的cur即为 pos，开始进行插入操作；
+	node->next = pos->next;
+	pos->next = node;
+
+
+}
+
+
+//11.删除在指定位置pos之前的元素，会影响到pos元素本身位置；若此时pos==*pphead，则删除pos自己，可以调用 Popfront()函数；		2必须得找到pre，但是单向链表是不能通过pos来找到pre的；所以得传头节点指针来遍历寻找
+//删除指定pos位置之前的一个元素pre。							
+void EraseBefore(SLNode** pphead, SLNode* pos)
+{
+	assert(pphead && pos&&*pphead);
+	//不可能存在*pphead==pos的情况，要特别处理；
+	
+	if (pos == *pphead)
+	{
+		//调用头删函数；
+		Popfront(pphead);
+	}
+	else         //找pos元素，并记录pre
+	{
+		SLNode* pre = *pphead;
+		//SLNode* cur = *pphead;    
+		while (pre->next != pos)
+		{
+			pre = pre->next;
+		}
+		
+		//找到了pre,开始操作
+		pre->next = pos->next;
+		free(pos);
+		pos = NULL;
+
+
+	}
+
+}
+
+
+
+//10.在指定位置pos 之后删除元素,,,,,,,，此时可以不传头节点的地址，，，直接通过pos来 删除;之前要传，那是因为你想删除pos元素，那你必须得找到pre，但是单向链表是不能通过pos来找到pre的；所以得传头节点指针来遍历寻找
+//12.删除指定pos位置之后的元素。
+void EraseAfter(SLNode* pos)
+{
+	assert(pos&&pos->next);
+	SLNode* del = pos->next;
+	pos->next = del->next;
+	//删除del；
+	free(del);
+	del = NULL;
+}
+
+
+//13.单链表的销毁
+void SLDestroy(SLNode** pphead)
+{
+
+	assert(pphead && *pphead);
+	SLNode* cur = *pphead;
+	//开始遍历，逐个一次free，最后将cur指针置为NULL；
+	while (cur)
+	{
+		SLNode* temp = cur;  
+		cur= (cur->next);
+		free(temp);
+
+	//更新cur
+	   
+
+
+	}
+	//将指向链表的头节点 *pphead  置为NULL,此时当前链表已被销毁；
+	*pphead = NULL;
+
+}
--- 链表_25_10_23/链表_25_10_23/test.c
+++ 链表_25_10_23/链表_25_10_23/test.c
@@ -4,15 +4,90 @@
 
 int main()
 {
+	//3.测试创建节点函数   和   打印函数
+	
 	SLNode* node1 = SLBuy(1);
 	SLNode* node2 = SLBuy(2);
 	SLNode* node3 = SLBuy(3);
+	
+	//4.测试尾插
 	Pushback(&node1, 67);
 	Pushback(&node1, 68);
-	SLPrint(NULL);
+	SLPrint(node1);
+	//5.测试头插
+	Pushfront(&node1, 456);
+	Pushfront(&node1, 897);
+		SLPrint(node1);
 
-
+	//6.测试尾删
+	Popback(&node1);
+	SLPrint(node1);
 	
-	//SLPrint(node1);
+
+	//7.测试头删
+	Popfront(&node1);
+	SLPrint(node1);
+
+	/*Popfront(&node1);
+	SLPrint(node1);*/
+
+
+	//8.测试 查找
+	SLNode* p = SLFind(&node1, 1);
+	//if (p != NULL)
+	//{
+	//	printf("%d\n", p->data);
+	//}
+	//else
+	//{
+	//	printf("没找到");
+	//}
+
+	//9.测试 在指定位置pos之前插入元素x;
+	SLInsertBefore(&node1,p, 57);
+	SLPrint(node1);
+	SLInsertBefore(&node1,p, 547);
+	SLPrint(node1);
+
+	SLNode* node5 = SLBuy(90);
+	    //pos之前插入要特别考虑的情况：*pphead==pos
+	SLInsertBefore(&node5, node5, 57);
+	SLPrint(node5);
+
+
+
+	//10.测试 在指定位置pos之后插入元素x;
+	SLInsertAfter( p, 8866);
+	SLPrint(node1);
+
+	SLInsertAfter(node5, 7789);
+	SLPrint(node5);
+
+//11.测试 删除pos之前的元素（即pos本身）
+EraseBefore(&node5,node5);
+SLPrint(node5);
+
+
+//12.测试 删除pos之后的元素；
+
+Pushback(&node5,4);
+Pushback(&node5,5);
+SLPrint(node5);
+EraseAfter(node5);
+EraseAfter(node5);
+SLPrint(node5);
+
+
+//13.测试销毁函数；
+SLDestroy(&node5);
+
+//方法一：直接打印，SLPrint函数会报错，此时链表为NULL；
+//SLPrint(node5);
+//方法二：调试观察；
+
+
 	return 0;
+
 }
+
+
